444d3c065977b587177a1a34a541298bbc1fcdb6
vishnu sivaprasadan <vishnu.sivaprasadan@gmail.com>
2024-03-02 16:15:27 +0530

    Add project files.

---
+++ crud/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private static List<User> _users = new List<User>();
+
+    [HttpPost]
+    public IActionResult CreateUser(User user)
+    {
+        user.Id = _users.Count + 1;
+        _users.Add(user);
+        return Ok(user);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetUser(int id)
+    {
+        var user = _users.FirstOrDefault(u => u.Id == id);
+        if (user == null)
+            return NotFound();
+
+        return Ok(user);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteUser(int id)
+    {
+        var user = _users.FirstOrDefault(u => u.Id == id);
+        if (user == null)
+            return NotFound();
+
+        _users.Remove(user);
+        return NoContent();
+    }
+
+    [HttpGet]
+    public IActionResult ListUsers()
+    {
+        return Ok(_users);
+    }
+}
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+}
